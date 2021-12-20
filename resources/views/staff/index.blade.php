e4a298a3eee3f885145591d854ee492963723445
adeyemionward <34043883+adeyemionward@users.noreply.github.com>
2021-12-20 16:36:06 +0100

    Update index.blade.php

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -19,7 +19,7 @@ tr:nth-child(even) {
   background-color: #dddddd;
 }
       </style>
-  @include('layouts.header')
+    @include('layouts.header')
     <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
         <div class="site-wrap">
             <div class="site-mobile-menu site-navbar-target">
@@ -33,36 +33,29 @@ tr:nth-child(even) {
            
             <br><br><br><br>
             <div class="col-lg-8 offset-lg-2">
-                <div class="btn btn-primary" style="margin-bottom: 20px;"><a href="{{route('staff.create')}}" style="color:#ffffff">Create Staff</a></div>
+                <div class="btn btn-primary" style="margin-bottom: 20px;"><a href="{{route('staff.create')}}" style="color:#ffffff">Create Staff</a>
+                </div>
           
-                    <div class="border rounded-lg" style="overflow-x:auto;">
+                <div class="border rounded-lg" style="overflow-x:auto;">
                 <table>
-                      <tr>
+                    <tr>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Position</th>
                      
-                      </tr>
-                      @foreach ($data as $key=> $value)
-                      <tr>
+                    </tr>
+                    @foreach ($data as $key=> $value)
+                    <tr>
                         <td>{{ ++$key}}</td>
                         <td>{{ $value->firstname }}</td>
                         <td>{{ $value->lastname }}</td>
                        
-                      </tr>
-                      @endforeach
-                      
-
-
-                    </table>
-
-            </div>
-            </div>
-
-               
-
+                    </tr>
+                    @endforeach
+                </table>
+                </div>
+            </div>     
         </div>
-        <script src="js/jquery-3.3.1.min.js"></script>
     </body>
 
 </html>
